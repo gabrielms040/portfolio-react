c734ffbfe52dad60c0dc70ce1f64b2ae6d6144aa
gabriel <162498294+gabrielms040@users.noreply.github.com>
2025-01-22 15:23:27 -0300

    links url  header

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,12 +1,17 @@
 import "./Header.css"
 
+const handleRedirect = (url) => {
+  window.open(url, "_blank");
+}
+
+
 const Header = () => {
   return (
     <header className='header'>
-   <button href="https://github.com/gabrielms040"> GitHub <img src="/img/github.png" alt="img github" /> </button>
-   <button href="https://www.linkedin.com/in/gabriel-morais123/"> Linkedin <img src="/img/linkedin.png" alt="img LinkedinInstagram" /> </button>
-   <button href="https://www.instagram.com/gabriel87.6/"> Instagram <img src="/img/instagram.png" alt="img Instagram" /> </button>
-   <button href="https://w.app/DQI4cC"> Whatsapp <img src="/img/whatsapp.png" alt="img Whatsapp" /> </button>
+   <button onClick={() => handleRedirect("https://github.com/gabrielms040")}> GitHub <img src="/img/github.png" alt="img github" /> </button>
+   <button onClick={() => handleRedirect("https://www.linkedin.com/in/gabriel-morais123/")}> Linkedin <img src="/img/linkedin.png" alt="img LinkedinInstagram" /> </button>
+   <button onClick={() => handleRedirect("https://www.instagram.com/gabriel87.6/")}> Instagram <img src="/img/instagram.png" alt="img Instagram" /> </button>
+   <button onClick={() => handleRedirect("https://w.app/DQI4cC")}> Whatsapp <img src="/img/whatsapp.png" alt="img Whatsapp" /> </button>
     
     </header>
   )
