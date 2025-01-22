e5f174c39113383d35d70e5e58eff1ea4556e97e
gabriel <162498294+gabrielms040@users.noreply.github.com>
2025-01-22 17:37:00 -0300

    .

--- src/SessionOne/index.jsx
+++ src/SessionOne/index.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import './SessionOne.css'
 
 const SessionOne = () => {
   return (
-    <div>
-        <h3>Bem vindo!</h3>
-        <img src="/img/icon_avatar.png" alt="Icon Avatar" />
-        <h2>Me chamo Gabriel e esse é meu Portfolio!</h2>
+  <div className="SessionOne">
+    <div className="Descricao">
+        <h2>Bem vindo!</h2>
+        <h3>Sou um desenvolvedor e estudante de Sistemas de Informação na Uniube, com experiência em React, JavaScript, Node.js e Python. Ativo no GitHub e LinkedIn, compartilho projetos, conecto-me com profissionais e acompanho as tendências do mercado de tecnologia.
+        Com habilidades em inglês e um forte foco em aprendizado contínuo, estou sempre buscando evoluir e contribuir para a criação de soluções eficientes e inovadoras!</h3>
     </div>
+    <div className="Avatar">
+        <img src="https://github.com/gabrielms040.png" alt="Icon Avatar" />
+        <h3>Seu desafio, nossa solução em código.</h3>
+    </div>
+  </div>
   )
 }
 
