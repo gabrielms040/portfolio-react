2ca2aac040bd0271520221dbd25d07c312fbec47
gabriel <162498294+gabrielms040@users.noreply.github.com>
2025-01-22 15:14:39 -0300

    links href header

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -3,10 +3,10 @@ import "./Header.css"
 const Header = () => {
   return (
     <header className='header'>
-   <button> GitHub <img src="/img/github.png" alt="img github" /> </button>
-   <button> Linkedin <img src="/img/linkedin.png" alt="img LinkedinInstagram" /> </button>
-   <button> Instagram <img src="/img/instagram.png" alt="img Instagram" /> </button>
-   <button> Whatsapp <img src="/img/whatsapp.png" alt="img Whatsapp" /> </button>
+   <button href="https://github.com/gabrielms040"> GitHub <img src="/img/github.png" alt="img github" /> </button>
+   <button href="https://www.linkedin.com/in/gabriel-morais123/"> Linkedin <img src="/img/linkedin.png" alt="img LinkedinInstagram" /> </button>
+   <button href="https://www.instagram.com/gabriel87.6/"> Instagram <img src="/img/instagram.png" alt="img Instagram" /> </button>
+   <button href="https://w.app/DQI4cC"> Whatsapp <img src="/img/whatsapp.png" alt="img Whatsapp" /> </button>
     
     </header>
   )
